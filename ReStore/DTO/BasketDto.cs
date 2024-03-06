e62c1e91da7a2d3d0e53e7a223b733d6e23e6cb2
Maksym <max.sheremeta228@gmail.com>
2024-03-06 18:52:31 +0200

    Add shopping cart feature

---
+++ ReStore/DTO/BasketDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Re_Store.DTO;
+
+public class BasketDto
+{
+    public int Id { get; set; }
+    public string BuyerId { get; set; }
+    public List<BasketItemDto> Items { get; set; }
+}
